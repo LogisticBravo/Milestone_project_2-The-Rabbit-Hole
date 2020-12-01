ab686f101e9a12c329130e06d30f15a04d4470a2
LogisticBravo <jay.jbradley@gmail.com>
2020-12-01 13:43:59 +0000

    modify typewriter effect code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,35 @@
-var i = 0;
-var txt = 'Would you like to play a game?';
-var txt2= "Ok, let's try this!";
+/* *** FOR DEBUG ON CLICK ***
+* var clicked = false
+* document.getElementById("yes").addEventListener("click",function(){clicked = true});
+*/
 
-/*$("#yes").click(function() {
-    $("#main-content").empty();
-});*/
+/* *** ORIGINAL TYPEWRITER CODE FROM W3 SCHOOLS - BASE FOR TYPEWRITE CODE ***
+*   function typeWriter() {
+*        if (i < txt.length) {
+*       document.getElementById("main-content").innerHTML += txt.charAt(i);
+*       i++;
+*       setTimeout(typeWriter, 70);
+*  }
+* }
+*/
 
-$("#yes").click(function() {
-    $("#main-content").empty();
-});
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("main-content").innerHTML += txt.charAt(i);
+function typeWriter(txt, i, divId) {
+  if (i < (txt.length)) {
+    $('#' + divId).html(txt.substring(0, i+1));
     i++;
-    setTimeout(typeWriter, 70);
-  }
-}
-window.onload = setTimeout(typeWriter, 5000);
+    setTimeout(function() {
+      typeWriter(txt, i, divId)
+    }, 70);
+  };
+};
 
+window.onload = setTimeout(function(){
+  var txt = $("#main-content").data("text");
+  typeWriter(txt, 0, "main-content");
+},5000);
 
+$(document).on("click","#yes", function(){
+  $("#main-content").empty();
+  var txt = $("#second").data("text");
+  typeWriter(txt, 0, "second");
+});
