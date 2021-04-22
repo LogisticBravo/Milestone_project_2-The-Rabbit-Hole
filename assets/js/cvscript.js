5d7ed38526e5228e7fda604764c65dc3474aa14c
LogisticBravo <jay.jbradley@gmail.com>
2021-04-22 02:02:45 +0000

    Add keyListen fn,arrayEquals fn & checkArray fn. Add arrays.

--- assets/js/cvscript.js
+++ assets/js/cvscript.js
@@ -3,6 +3,7 @@ window.onload = function(){
     $("h6").parent("td").removeClass("pr-5");
     $("tr td:nth-child(3)").addClass("pl-5");
     $("h6").parent("td").removeClass("pl-5");
+    keyListen();
     quickType("asciiname");
     setTimeout(function () {
             quickType("edu1")
@@ -116,3 +117,53 @@ function quickType(spanId,optionalVariable ='',optionalValue = ''){
         var txt = $("#" + spanId).data("text") + optionalVariable + optionalValue;
         typeWriter(txt, 0, spanId);
     }
+
+let riddlesAns ={
+    riddle1: "up",
+    riddle2: "up",
+    riddle3: "down",
+    riddle4: "down",
+    riddle5: "left",
+    riddle6: "right",
+    riddle7: "left",
+    riddle8: "right",
+    riddle9: "b",
+    riddle10: "a",
+}
+
+let konami = [];
+
+konamiCount = 0;
+
+function keyListen (){
+            $("div").last().after('<p id="log"></p>');
+            $("#log").hide();
+            document.addEventListener('keydown', logKey); //Keyboard event listener for the input of the code
+                function logKey(e) {
+                    log.textContent += ` ${e.code}`;
+                    kcode = document.getElementById("log").innerHTML;
+                    konami = kcode.replace(/Arrow/g,'').replace(/Key/g,'').split(" "); //removes the words 'Arrow' and 'Key' from the Array
+                    konami.shift(); 
+                    konami.length = 10; //Restricts the length of the Array to 10 so that the first 10 key strokes only are added to the array
+                    if(konami.includes("Up","0") == true && konami.includes("Up","2") == false && konami.includes("A","9") == true){konamiCount += 1};
+                    if(konamiCount > 0){checkArray();};
+                    console.log(konami);
+};
+}
+
+function arrayEquals(a, b) { //Compare two arrays to check they are the same (Attributed to: https://masteringjs.io/tutorials/fundamentals/compare-arrays)
+  if (Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index]))
+    alert("Correct");
+else alert("wrong");
+}
+
+function checkArray (){
+    for (let i = 0; i < konami.length; i++) {
+        konami[i] = konami[i].toLowerCase();
+}
+        riddlesAns = Object.values(riddlesAns); //converts the objects property values to an array
+        arrayEquals(riddlesAns,konami); //calls upon the arrayEquals function to ensure that the keyboard input matches the correct riddle answers i.e. the konami code
+}
